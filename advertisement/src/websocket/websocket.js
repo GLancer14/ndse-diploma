3244ed8089becaca13b12e67fe67661643813b5f
unknown <181259602+GLancer14@users.noreply.github.com>
2025-09-11 12:54:14 +0400

    Complete working version of socket
    For new and existing chats

--- advertisement/src/websocket/websocket.js
+++ advertisement/src/websocket/websocket.js
@@ -9,13 +9,11 @@ function createSocketConnection(io) {
     const { userId } = socket.handshake.query;
 
     const userChats = await Chats.findAllUsersChats(userId);
-    // console.log("CHats count", userChats.length)
 
     userChats.forEach((chat) => {
       socket.join(`${userId}/${chat.id}`);
       const subscribeListener = (chatId, message) => {
         if (chatId === chat.id) {
-          console.log("Message:", message)
           const recevier = chat.users.filter(user => user !== message.author);
           socket.to(`${recevier}/${chat.id}`).emit("newMessage", message);
           socket.emit("newMessage", message);
@@ -27,8 +25,6 @@ function createSocketConnection(io) {
     });
 
     socket.on("getHistory", async (receiverId) => {
-      // const senderId = JSON.parse(Object.values(socket.request.sessionStore.sessions)[0]).passport.user;
-      // const usersIdsParsed = JSON.parse(usersIds)
       const chat = await Chats.find([userId, receiverId]);
       socket.emit("chatHistory", JSON.stringify(chat));
     });
@@ -42,18 +38,36 @@ function createSocketConnection(io) {
       } else {
         newMessage = await Chats.sendMessage(messageDataParsed);
         chat = await Chats.find([messageDataParsed.author, messageDataParsed.recevier]);
+
         socket.join(`${userId}/${chat.id}`);
-        const subscribeListener = (chatId, message) => {
+        const authorSubscribeListener = (chatId, message) => {
           if (chatId === chat.id) {
             console.log("New chat's message:", message)
 
-            socket.to(`${messageDataParsed.recevier}/${chat.id}`).emit("newMessage", message);
+            const recevier = chat.users.filter(user => user !== message.author);
+            socket.to(`${recevier}/${chat.id}`).emit("newMessage", message);
             socket.emit("newMessage", message);
           }
         };
 
-        subscribeListenersStorage.set(`${userId}/${chat.id}`, subscribeListener);
-        Chats.subscribe(`${userId}/${chat.id}`, subscribeListener);
+        let recevierSocket = Array.from(io.sockets.sockets).find(socket => {
+          return messageDataParsed.recevier === socket[1].handshake.query.userId
+        })[1];
+
+        recevierSocket.join(`${messageDataParsed.recevier}/${chat.id}`);
+        const recevierSubscribeListener = (chatId, message) => {
+          if (chatId === chat.id) {
+            const recevier = chat.users.filter(user => user === message.author);
+            recevierSocket.to(`${recevier}/${chat.id}`).emit("newMessage", message);
+            recevierSocket.emit("newMessage", message);
+          }
+        };
+
+        subscribeListenersStorage.set(`${userId}/${chat.id}`, authorSubscribeListener);
+        subscribeListenersStorage.set(`${messageDataParsed.recevier}/${chat.id}`, recevierSubscribeListener);
+
+        Chats.subscribe(`${userId}/${chat.id}`, authorSubscribeListener);
+        Chats.subscribe(`${messageDataParsed.recevier}/${chat.id}`, recevierSubscribeListener);
       }
 
       subscribeEmitter.emit(`${userId}/${chat.id}`, chat.id, newMessage);
@@ -62,9 +76,7 @@ function createSocketConnection(io) {
     socket.on("disconnect", async () => {
       console.log(`Socket disconnected: ${id}`);
       const userChats = await Chats.findAllUsersChats(userId);
-      console.log(userChats)
       userChats.forEach(chat => {
-
         // const subscribeListeners = subscribeListenersStorage.get(chat.id);
         const subscribeListener = subscribeListenersStorage.get(`${userId}/${chat.id}`);
         // subscribeListeners.forEach(listener => {
